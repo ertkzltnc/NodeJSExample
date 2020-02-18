44594fe2e0a4ff5694b79fdaff4c4f6dc4854b9a
ertkzltnc <ertkzltnc@gmail.com>
2020-02-18 20:07:48 +0300

    Debug

--- NodeJSDebugExercise/index.js
+++ NodeJSDebugExercise/index.js
@@ -3,7 +3,16 @@ var bike=require("./bike");
 
 var Toyota=new car();
 
+
 Toyota.printType();
 Toyota.printSpeed();
 Toyota.speedUp();
 Toyota.printSpeed();
+
+// var Bianchi=new bike();
+
+
+// Bianchi.printSpeed();
+// Bianchi.speedUp();
+// Bianchi.printSpeed();
+
